43878511ca1dc1d550f2da788ca36578b519a7fb
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-08-25 20:48:54 +0600

    fix hotels page header style

--- client_code/src/pages/Hotels/index.jsx
+++ client_code/src/pages/Hotels/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/Header';
+import Navbar from '../../components/Navbar';
 
 export default function index() {
   return (
-    <div>Hotels</div>
+    <div>
+      <Navbar />
+      <Header type="hotels"/>
+    </div>
   )
 }
