40bc83b6e377544725bf7f09e102c8a134753260
mnin-nahid <nahidulislam.nayan54@gmail.com>
2022-08-26 00:06:16 +0600

    added featured and property components

--- client_code/src/pages/Home/index.jsx
+++ client_code/src/pages/Home/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Navbar from "../../components/Navbar";
+import Featured from "../../components/Featured";
+import Property from "../../components/Property";
+import './index.scss';
 
 export default function Home() {
   return (
     <div>
-        < Navbar />
-        < Header />
+      < Navbar />
+      < Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">
+          Browse by property type
+        </h1>
+        < Property />
+      </div>
     </div>
   )
 }
