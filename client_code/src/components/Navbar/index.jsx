8797e0e787295a901b89b51cb67c61b8bd9ee354
mnin-nahid <nahidulislam.nayan54@gmail.com>
2023-08-31 13:44:19 +0600

    change domain

--- client_code/src/components/Navbar/index.jsx
+++ client_code/src/components/Navbar/index.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navContainer">
-            <span className='logo'> <Link to="/">MUSAFIR.COM</Link></span>
+            <span className='logo'> <Link to="/">MUSAFIR</Link></span>
             <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
